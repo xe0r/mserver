68c7cc1c68534c72906d26e9745e2565063e4205
Leonid Kaplan <xe0r@users.noreply.github.com>
2016-01-09 19:55:59 +0300

    Hide dirs

--- player.js
+++ player.js
@@ -1,228 +1,241 @@
-function Player(node) {
-  this.len = 0;
-  this.filename = "";
-  this.url = "";
-
-  this.startPos = 0;
-
-  this.node = node;
-
-  this.video = node.querySelector('video');
-
-  this.controls = node.querySelector('.controls');
-
-  this.progress = node.querySelector('.progress');
-  this.p1 = this.progress.querySelector('div');
-
-  function isFullscreen() {
-    if (node.mozRequestFullScreen) {
-      return document.mozFullScreenElement != null;
-    } else if (node.webkitRequestFullscreen) {
-      return document.webkitFullscreenElement != null;
+(function(window, document) {
+  function toggleVisible(elem) {
+    if (elem.style.display=='none') {
+      elem.style.display='block';
     } else {
-      return document.fullscreenElement != null;
+      elem.style.display='none';
     }
   }
 
-  function updateSize() {
-    console.log(this.video.videoWidth, this.video.videoHeight);
-    if (!isFullscreen()) {
-      var w = this.video.videoWidth;
-      var h = this.video.videoHeight;
-      if (!w || !h) {
-        w = 600;
-        h = 300;
+  function HomeMediaPlayer(node) {
+    this.len = 0;
+    this.filename = "";
+    this.url = "";
+
+    this.startPos = 0;
+
+    this.node = node;
+
+    this.video = node.querySelector('video');
+
+    this.controls = node.querySelector('.controls');
+
+    this.progress = node.querySelector('.progress');
+    this.p1 = this.progress.querySelector('div');
+
+    function isFullscreen() {
+      if (node.mozRequestFullScreen) {
+        return document.mozFullScreenElement != null;
+      } else if (node.webkitRequestFullscreen) {
+        return document.webkitFullscreenElement != null;
+      } else {
+        return document.fullscreenElement != null;
       }
+    }
 
-      var maxw = 800;
-      if (w > maxw) {
-        h = parseInt(h * maxw/w);
-        w = maxw;
+    function updateSize() {
+      console.log(this.video.videoWidth, this.video.videoHeight);
+      if (!isFullscreen()) {
+        var w = this.video.videoWidth;
+        var h = this.video.videoHeight;
+        if (!w || !h) {
+          w = 600;
+          h = 300;
+        }
+
+        var maxw = 800;
+        if (w > maxw) {
+          h = parseInt(h * maxw/w);
+          w = maxw;
+        }
+        node.style.width = w+'px';
+        node.style.height = h+'px';
+      } else {
+        node.style.width = '100%';
+        node.style.height = '100%';
       }
-      node.style.width = w+'px';
-      node.style.height = h+'px';
-    } else {
-      node.style.width = '100%';
-      node.style.height = '100%';
     }
-  }
-  updateSize = updateSize.bind(this);
+    updateSize = updateSize.bind(this);
 
-  function updatePos() {
-    if (this.len == 0) {
-      this.p1.style.width = "0px";
-      return;
+    function updatePos() {
+      if (this.len == 0) {
+        this.p1.style.width = "0px";
+        return;
+      }
+      var total = this.progress.offsetWidth;
+      var time = this.startPos + this.video.currentTime;
+      var r = time/this.len;
+      //console.log("pos", this.startPos, this.video.currentTime, r);
+      var x = parseInt(total*r);
+      this.p1.style.width = x + "px";
     }
-    var total = this.progress.offsetWidth;
-    var time = this.startPos + this.video.currentTime;
-    var r = time/this.len;
-    //console.log("pos", this.startPos, this.video.currentTime, r);
-    var x = parseInt(total*r);
-    this.p1.style.width = x + "px";
-  }
-  updatePos = updatePos.bind(this);
+    updatePos = updatePos.bind(this);
 
-  function onFullscreenChange(evt) {
-    updateSize();
-    updatePos();
-  }
-
-  document.addEventListener('fullscreenchange', onFullscreenChange, false);
-  document.addEventListener('mozfullscreenchange', onFullscreenChange, false);
-  document.addEventListener('webkitfullscreenchange', onFullscreenChange, false);
-
-  function enableFullscreen() {
-    if (node.mozRequestFullScreen) {
-      node.mozRequestFullScreen()
-    } else if (node.webkitRequestFullscreen) {
-      node.webkitRequestFullscreen();
-    } else {
-      node.requestFullscreen();
+    function onFullscreenChange(evt) {
+      updateSize();
+      updatePos();
     }
-  }
 
-  function disableFullscreen() {
-    if (node.mozRequestFullScreen) {
-      document.mozCancelFullScreen()
-    } else if (node.webkitRequestFullscreen) {
-      document.webkitCancelFullScreen();
-    } else {
-      document.cancelFullscreen();
+    document.addEventListener('fullscreenchange', onFullscreenChange, false);
+    document.addEventListener('mozfullscreenchange', onFullscreenChange, false);
+    document.addEventListener('webkitfullscreenchange', onFullscreenChange, false);
+
+    function enableFullscreen() {
+      if (node.mozRequestFullScreen) {
+        node.mozRequestFullScreen()
+      } else if (node.webkitRequestFullscreen) {
+        node.webkitRequestFullscreen();
+      } else {
+        node.requestFullscreen();
+      }
     }
-    updateSize();
-  }
 
-  function toggleFullscreen() {
-    if (isFullscreen()) {
-      disableFullscreen();
-    } else {
-      enableFullscreen();
+    function disableFullscreen() {
+      if (node.mozRequestFullScreen) {
+        document.mozCancelFullScreen()
+      } else if (node.webkitRequestFullscreen) {
+        document.webkitCancelFullScreen();
+      } else {
+        document.cancelFullscreen();
+      }
+      updateSize();
     }
-  }
 
-  function togglePause() {
-    if (this.video.paused) {
-      this.video.play();
-    } else {
-      this.video.pause();
+    function toggleFullscreen() {
+      if (isFullscreen()) {
+        disableFullscreen();
+      } else {
+        enableFullscreen();
+      }
     }
-  }
-  togglePause = togglePause.bind(this);
 
-  this.controls.addEventListener('click', function(evt) {
-    togglePause();
-  }, false);
-
-  this.controls.querySelector('.fullscreen').addEventListener('click', function(evt) {
-    toggleFullscreen();
-    evt.stopPropagation();
-  }, false);
-
-  this.video.addEventListener('canplay', function(evt) {
-    updateSize();
-  }, false);
-  this.video.addEventListener('timeupdate', function(evt) {
-    updatePos();
-  }, false);
-
-  this.progress.addEventListener('click', (function(evt) {
-    if (this.len == 0) {
-      return;
+    function togglePause() {
+      if (this.video.paused) {
+        this.video.play();
+      } else {
+        this.video.pause();
+      }
     }
-    console.log(evt);
-    var total = this.progress.offsetWidth;
-    var r = evt.layerX / total;
-    console.log(r);
-    this.seek(this.len*r);
-    evt.stopPropagation();
-  }).bind(this), false);
-}
+    togglePause = togglePause.bind(this);
 
-Player.prototype.seek = function(pos) {
-  this.video.pause();
-  this.video.src = this.url+parseInt(pos*100);
-  console.log("Seeking to", this.video.src);
-  this.startPos = pos;
-  this.video.play();
-}
+    this.controls.addEventListener('click', function(evt) {
+      togglePause();
+    }, false);
 
-Player.prototype.setFilename = function(filename) {
-  if (this.xhr) {
-    this.xhr.abort();
-    this.xhr = null;
+    this.controls.querySelector('.fullscreen').addEventListener('click', function(evt) {
+      toggleFullscreen();
+      evt.stopPropagation();
+    }, false);
+
+    this.video.addEventListener('canplay', function(evt) {
+      updateSize();
+    }, false);
+    this.video.addEventListener('timeupdate', function(evt) {
+      updatePos();
+    }, false);
+
+    this.progress.addEventListener('click', (function(evt) {
+      if (this.len == 0) {
+        return;
+      }
+      console.log(evt);
+      var total = this.progress.offsetWidth;
+      var r = evt.layerX / total;
+      console.log(r);
+      this.seek(this.len*r);
+      evt.stopPropagation();
+    }).bind(this), false);
   }
-  this.video.src = "";
 
-  this.len = 0;
-  this.filename = filename;
+  HomeMediaPlayer.prototype.seek = function(pos) {
+    this.video.pause();
+    this.video.src = this.url+parseInt(pos*100);
+    console.log("Seeking to", this.video.src);
+    this.startPos = pos;
+    this.video.play();
+  }
 
-  this.url = '/video/' + filename + '?ts=';
-
-  var iurl = '/info/' + filename;
-  this.xhr = new XMLHttpRequest();
-  this.xhr.open('GET', iurl, true)
-  this.xhr.onreadystatechange = function() { 
-    if (this.xhr.readyState == XMLHttpRequest.DONE && this.xhr.status == 200) {
-      var resp = JSON.parse(this.xhr.response);
-      console.log(resp);
-      this.len = parseFloat(resp.format.duration);
-      console.log("Length:", this.len);
+  HomeMediaPlayer.prototype.setFilename = function(filename) {
+    if (this.xhr) {
+      this.xhr.abort();
       this.xhr = null;
     }
-  }
-  this.xhr.onreadystatechange = this.xhr.onreadystatechange.bind(this);
-  this.xhr.send();
+    this.video.src = "";
 
-  this.seek(0);
-}
+    this.len = 0;
+    this.filename = filename;
 
-window.addEventListener('load', function(e){
-  var player = new Player(document.getElementById('node'));
+    this.url = '/video/' + filename + '?ts=';
 
-  function fileClicked(evt) {
-    player.setFilename(evt.target.dataset.filename);
-  }
-
-  function dirClicked(evt) {
-    var d1 = evt.target.parentNode;
-    var content = d1.querySelector('.dircontent');
-    if (content)
-      return;
-    content = document.createElement('div');
-    content.className='dircontent';
-    d1.appendChild(content);
-    fetchDirinfo(evt.target.dataset.dirname, content);
-  }
-
-  function fetchDirinfo(dir, elem) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/dirinfo/" + dir, true)
-    xhr.onreadystatechange = function() { 
-      if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-        var resp = JSON.parse(xhr.response);
+    var iurl = '/info/' + filename;
+    this.xhr = new XMLHttpRequest();
+    this.xhr.open('GET', iurl, true)
+    this.xhr.onreadystatechange = (function() { 
+      if (this.xhr.readyState == XMLHttpRequest.DONE && this.xhr.status == 200) {
+        var resp = JSON.parse(this.xhr.response);
         console.log(resp);
-        for (dn of resp.dirs) {
-          var d1 = document.createElement('div');
-          var d = document.createElement('div');
-          d.className = "dirinfo";
-          d.dataset.dirname = dir + "/" + dn;
-          d.appendChild(document.createTextNode(dn));
-          d1.appendChild(d);
-          elem.appendChild(d1);
-          d.addEventListener('click', dirClicked, false);
-        }
-        for (fn of resp.files) {
-          var d = document.createElement('div');
-          d.className = "fileinfo";
-          d.dataset.filename = dir + "/" + fn;
-          d.appendChild(document.createTextNode(fn));
-          elem.appendChild(d);
-          d.addEventListener('click', fileClicked, false);
+        this.len = parseFloat(resp.format.duration);
+        console.log("Length:", this.len);
+        this.xhr = null;
+      }
+    }).bind(this);
+    this.xhr.send();
+
+    this.seek(0);
+  }
+
+  window.HomeMediaPlayer = HomeMediaPlayer;
+
+  window.addEventListener('load', function(e){
+    var player = new HomeMediaPlayer(document.getElementById('node'));
+
+    function fileClicked(evt) {
+      player.setFilename(evt.target.dataset.filename);
+    }
+
+    function dirClicked(evt) {
+      var d1 = evt.target.parentNode;
+      var content = d1.querySelector('.dircontent');
+      if (content) {
+        toggleVisible(content);
+        return;
+      }
+      content = document.createElement('div');
+      content.className='dircontent';
+      d1.appendChild(content);
+      fetchDirinfo(evt.target.dataset.dirname, content);
+    }
+
+    function fetchDirinfo(dir, elem) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', "/dirinfo/" + dir, true)
+      xhr.onreadystatechange = function() { 
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+          var resp = JSON.parse(xhr.response);
+          console.log(resp);
+          for (dn of resp.dirs) {
+            var d1 = document.createElement('div');
+            var d = document.createElement('div');
+            d.className = "dirinfo";
+            d.dataset.dirname = dir + "/" + dn;
+            d.appendChild(document.createTextNode(dn));
+            d1.appendChild(d);
+            elem.appendChild(d1);
+            d.addEventListener('click', dirClicked, false);
+          }
+          for (fn of resp.files) {
+            var d = document.createElement('div');
+            d.className = "fileinfo";
+            d.dataset.filename = dir + "/" + fn;
+            d.appendChild(document.createTextNode(fn));
+            elem.appendChild(d);
+            d.addEventListener('click', fileClicked, false);
+          }
         }
       }
+      xhr.send();
     }
-    xhr.send();
-  }
-  fetchDirinfo('', document.getElementById('leftbar'));
+    fetchDirinfo('', document.getElementById('leftbar'));
 
-}, false);
+  }, false);
+})(window, document);
